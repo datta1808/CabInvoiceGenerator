42ae9ac65c514f9ccce3fd9a039370553536726f
datta1808 <bagaledatta18@gmail.com>
2021-06-18 12:41:15 +0530

    ADDED : Function to get Enhanced Invoice

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -1,30 +1,42 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+
+    InvoiceGenerator invoiceGenerator =null;
+
+    @Before
+    public void setUp() throws Exception {
+        invoiceGenerator=new InvoiceGenerator();
+    }
+
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
 
+
     @Test
     public void givenLessDistanceAndTime_shouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
 
+
     @Test
     public void givenMultipleRides_shouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
         Ride[] rides = { new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
